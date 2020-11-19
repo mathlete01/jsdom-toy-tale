7de1912d2fcb53b4c9e4b1205f143fb6fdaff767
mathlete01 <msallin@gmail.com>
2020-11-19 14:22:12 -0800

    exercise 1-3 works

--- src/index.js
+++ src/index.js
@@ -12,4 +12,39 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetchToys()
 });
+
+function fetchToys(){
+  return fetch("http://localhost:3000/toys")
+    .then (response => response.json())
+    // .then (response => console.log(response.toys))
+    .then (response => addToys(response))
+}
+
+function addToys(array){
+  let coll = document.getElementById("toy-collection")
+  for(let i = 0; i< array.length; i++){
+    let newDiv = document.createElement("div")
+    newDiv.setAttribute("class", "card")
+    addToyInfo(newDiv, array[i])
+    coll.appendChild(newDiv)
+  }
+}
+
+function addToyInfo(div, toy){
+  let h2 = document.createElement("h2")
+  h2.innerText = toy.name
+  let img = document.createElement("img")
+  img.setAttribute("src", toy.image)
+  img.setAttribute("class", "toy-avatar")
+  let p = document.createElement("p")
+  p.innerText = toy.likes
+  let button = document.createElement("button")
+  button.setAttribute("class", "like-btn")
+  button.innerText = "Like"
+  div.appendChild(h2)
+  div.appendChild(img)
+  div.appendChild(p)
+  div.appendChild(button)
+}
